9064a1b7733383176123d568f1804fb69c00afd9
Crustacean87! <declanv@gmail.com>
2014-07-05 15:49:55 -0400

    Adding in database table for schema file

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE posts
+  (
+    id serial NOT NULL primary key,
+    input_1 varchar(255),
+    input_2 varchar(255),
+    input_3 varchar(255),
+    input_4 varchar(255),
+    input_5 varchar(255)
+    );
