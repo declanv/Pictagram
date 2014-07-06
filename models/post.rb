63d967f43a5102fa2a612cacef8b518c2459c595
Crustacean87! <declanv@gmail.com>
2014-07-06 19:23:42 -0400

    Removed post model API logic since it was redundant of the logic in the Instagram module

--- models/post.rb
+++ models/post.rb
@@ -1,10 +1,3 @@
 class Post < ActiveRecord::Base
 
-  def get_photo(word)
-    url = "https://api.instagram.com/v1/tags/#{word}/media/recent?access_token=INSTA_ACCESS_TOKEN"
-    response = HTTParty.get(url)
-    image_url = response["data"][0]["images"]["standard_resolution"]["url"]
-    image_url
-  end
-
 end
