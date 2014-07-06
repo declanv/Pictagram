6e18d75a11f81ec4bcb47210fd8308d7fbeab133
Crustacean87! <declanv@gmail.com>
2014-07-06 19:06:05 -0400

    Added in post form in index page, along with the post method in the main.rb file

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'active_record'
 require 'pg'
 require 'httparty'
 
+
 # MODELS
 require_relative 'models/post'
 
@@ -17,8 +18,28 @@ ActiveRecord::Base.establish_connection({
   })
 
 get '/' do
-  # @posts = Post.order(id: :desc)
-  # # # @posts = Post.all
-  # @post = @posts.first
+  @posts = Post.order(id: :desc)
+  # # @posts = Post.all
+  @post = @posts.first
   erb :index
+end
+
+post '/posts' do
+
+  input_1 = Instagram.get_photo(params[:input_1])
+  input_2 = params[:input_2]
+  input_3 = Instagram.get_photo(params[:input_3])
+  input_4 = params[:input_4]
+  input_5 = Instagram.get_photo(params[:input_5])
+  Post.create(
+    input_1: input_1,
+    input_2: input_2,
+    input_3: input_3,
+    input_4: input_4,
+    input_5: input_5
+  )
+  @posts = Post.order(id: :desc)
+  @created = "created"
+  @post = @posts.first
+  redirect "/"
 end
